4791c3fc70dcc40d746433bd8a8b1ec89bde4dea
Mikefrfr <mohitsusangeorge@gmail.com>
2025-01-15 18:51:34 +0530

    made some upates

--- theme/js/script.js
+++ theme/js/script.js
@@ -166,3 +166,18 @@ document.getElementById('contactForm').addEventListener('submit', async (event) 
 	}
   });
   
+
+  
+//video playing
+const video = document.getElementById('myVideo');
+const playPauseBtn = document.getElementById('playPauseBtn');
+
+playPauseBtn.addEventListener('click', () => {
+    if (video.paused) {
+        video.play();
+        playPauseBtn.textContent = 'Pause';
+    } else {
+        video.pause();
+        playPauseBtn.textContent = 'Play';
+    }
+});
